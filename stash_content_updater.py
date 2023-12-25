07f696353c6d62320661b638c820678725ff08af
Internet Guru <internet_guru99@proton.me>
2023-12-25 11:50:59 -0300

    Adding scene updater

--- stash_content_updater.py
+++ stash_content_updater.py
@@ -70,6 +70,47 @@ def find_images(performer_name):
 
     return make_graphql_request(query_images)
 
+def find_scenes(performer_name):
+    query_scenes = f'''
+        query {{
+          findScenes (
+            scene_filter: {{
+              path: {{
+                value: "Z:Testing\\\\Exclusive Content\\\\{performer_name}\\\\",
+                modifier: INCLUDES
+              }}
+            }}
+          ) {{
+            count,
+            scenes {{
+              id,
+              studio {{
+                id,
+                name,
+                parent_studio {{
+                  id,
+                  name
+                }},
+                scene_count,
+                image_count,
+              }},
+              tags {{
+                id,
+                name,
+                description,
+                parents {{
+                	id,
+                  name
+                }}, 
+                image_count
+              }}
+            }}
+          }}
+        }}
+    '''
+
+    return make_graphql_request(query_scenes)
+
 def bulk_update_images(image_ids, performer_id):
     ids_str = ", ".join(map(str, image_ids))
     query_bulk_update = f'''
@@ -90,7 +131,28 @@ def bulk_update_images(image_ids, performer_id):
         }}
     '''
 
-    print(query_bulk_update)
+    return make_graphql_request(query_bulk_update)
+
+def bulk_update_scenes(scene_ids, performer_id):
+    ids_str = ", ".join(map(str, scene_ids))
+    query_bulk_update = f'''
+        mutation {{
+          bulkSceneUpdate(
+            input: {{
+              ids: [{ids_str}],
+              performer_ids: {{
+                mode: ADD,
+                ids: [{performer_id}]
+              }}
+            }}
+          ) {{
+            id,
+            title,
+            date
+          }}
+        }}
+    '''
+
     return make_graphql_request(query_bulk_update)
 
 def process_folders(base_folder):
@@ -115,6 +177,16 @@ def process_folders(base_folder):
         image_ids = [image["id"] for image in response_images["data"]["findImages"]["images"]]
 
         bulk_update_images(image_ids, performer_id)
+
+        response_scenes = find_scenes(performer_name)
+        scene_count = response_scenes.get("data", {}).get("findScenes", {}).get("count", 0)
+
+        if scene_count == 0:
+            continue
+
+        scene_ids = [scene["id"] for scene in response_scenes["data"]["findScenes"]["scenes"]]
+
+        bulk_update_scenes(scene_ids, performer_id)
 
 base_folder = "/volumes/Documents/Testing/Exclusive Content"
 
